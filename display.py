b32d83cdf763cc51edfdf353bfe303d5ef335078
JohnProgramming08 <157177709+JohnProgramming08@users.noreply.github.com>
2024-01-23 19:01:17 +0000

    Improved UI, not finished probably

--- display.py
+++ display.py
@@ -5,32 +5,39 @@ class Display:
   #sets up initial window and its settings
   def __init__(self):
     self.root = tk.Tk()
-    self.root.title("Quote Generator")
+    self.root.title("Quote Finder")
     self.root.geometry("300x300")
     self.root.resizable(False, False)
     self.root.configure(bg = "white")
 
   #creates the frames that are going to be used in the program
   def frames(self):
-    self.quote_frame = tk.LabelFrame(self.root, padx = 10, pady = 10)
-    self.quote_frame.grid(row = 4, column = 0)
+    self.search_frame = tk.LabelFrame(self.root, bg = "white")
+    self.search_frame.grid(row = 1, column = 0, columnspan = 3)
+    
+    self.quote_frame = tk.LabelFrame(self.root, bg = "white", padx = 10, pady = 10)
+    self.quote_frame.grid(row = 5, column = 1)
 
   #creates the widgets used to generate/find quotes
-  def heading(self):
-    self.title = tk.Label(self.root, text = "Quote Generator")
-    self.title.grid(row = 0, column = 0)
+  def heading(self, search):
+    self.title = tk.Label(self.root, text = "Quote Finder", bg = "white")
+    self.title.grid(row = 0, column = 0, columnspan = 3)
     
-    self.search_bar = tk.Entry(self.root)
-    self.search_bar.grid(row = 1, column = 0)
+    self.search_bar = tk.Entry(self.search_frame)
+    self.search_bar.grid(row = 0, column = 0, ipadx = 30)
+    self.search_bar.insert(0, "enter a tag that describes the quote")
 
-    self.search_button = tk.Button(self.root, text = "Search")
-    self.search_button.grid(row = 1, column = 1)
-
-    self.random_button = tk.Button(self.root, text = "Generate Random Quote")
-    self.random_button.grid(row = 2, column = 0)
+    self.search_button = tk.Button(self.search_frame, text = "Search")
+    self.search_button.grid(row = 0, column = 1)
 
     self.empty_row1 = tk.Label(self.root, text = "", bg = "white")
-    self.empty_row1.grid(row = 3, column = 0)
+    self.empty_row1.grid(row = 2, column = 0, columnspan = 3)
+    
+    self.random_button = tk.Button(self.root, text = "Generate Random Quote", command = search)
+    self.random_button.grid(row = 3, column = 0, sticky = "w", columnspan = 3)
+
+    self.empty_row2 = tk.Label(self.root, text = "", bg = "white")
+    self.empty_row2.grid(row = 4, column = 0)
 
   #creates the widget for displaying the quotes and scrolling through them
   def quote_display(self, next, back):
@@ -38,14 +45,16 @@ class Display:
     self.quote_label.grid(row = 0, column = 0)
 
     self.next_button = tk.Button(self.root, text = ">", command = next)
-    self.next_button.grid(row = 5, column = 0, sticky = "e")
+    self.next_button.grid(row = 6, column = 1, sticky = "e")
 
     self.back_button = tk.Button(self.root, text = "<", command = back)
-    self.back_button.grid(row = 5, column = 0, sticky = "w")
+    self.back_button.grid(row = 6, column = 1, sticky = "w")
 
+  
+  
 #testing purposes
 display = Display()
 display.frames()
-display.heading()
+display.heading("search")
 display.quote_display("next", "back")
 display.root.mainloop()
